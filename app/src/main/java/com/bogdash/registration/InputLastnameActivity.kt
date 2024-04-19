4fdd521e2fcf937d846479622cddfdb29734f64e
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-04-19 12:39:07 +0300

    Update design for InputLastnameActivity

--- app/src/main/java/com/bogdash/registration/InputLastnameActivity.kt
+++ app/src/main/java/com/bogdash/registration/InputLastnameActivity.kt
@@ -1,5 +1,6 @@
 package com.bogdash.registration
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -8,6 +9,7 @@ import androidx.core.view.WindowInsetsCompat
 import com.bogdash.registration.databinding.ActivityInputLastnameBinding
 
 class InputLastnameActivity : AppCompatActivity() {
+    private lateinit var lastname: String
 
     private lateinit var binding: ActivityInputLastnameBinding
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,10 +18,25 @@ class InputLastnameActivity : AppCompatActivity() {
 
         binding = ActivityInputLastnameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.inputLastname) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        binding.buttonInputLastnameNext.setOnClickListener {
+            getLastname()
+            initIntent()
+        }
+    }
+
+    private fun getLastname() {
+        lastname = binding.editTextInputLastname.text.toString()
+    }
+
+    private fun initIntent() {
+        val intent = Intent(this, InputAgeActivity::class.java)
+        startActivity(intent)
     }
 }
