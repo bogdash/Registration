48d8cd87deaa31df8548982caa151ed51ba101d4
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-04-19 12:03:41 +0300

    Update design for RegistrationActivity

--- app/src/main/java/com/bogdash/registration/RegistrationActivity.kt
+++ app/src/main/java/com/bogdash/registration/RegistrationActivity.kt
@@ -1,5 +1,6 @@
 package com.bogdash.registration
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -20,6 +21,11 @@ class RegistrationActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.buttonStartRegistration.setOnClickListener {
+            val intent = Intent(this, InputNameActivity::class.java)
+            startActivity(intent)
         }
     }
 }
