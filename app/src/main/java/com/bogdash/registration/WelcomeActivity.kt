d7f4510bd4a7791c98aa3ec1edf46f78d8070256
Anastasia <ms.anastasia.bogdanova@gmail.com>
2024-04-19 12:49:39 +0300

    Update design for WelcomeActivity

--- app/src/main/java/com/bogdash/registration/WelcomeActivity.kt
+++ app/src/main/java/com/bogdash/registration/WelcomeActivity.kt
@@ -16,6 +16,7 @@ class WelcomeActivity : AppCompatActivity() {
 
         binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(binding.welcome) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
